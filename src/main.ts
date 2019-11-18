b9433a9232c7c5dacf4907df29a0f8a10e8c50f5
Adrien <adrien.masson@hotmail.fr>
2019-11-18 14:00:31 +0100

    express init 'Hello'

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
-export function helloWorld(lang = 'Typescript'): string {
-    return `🦁 I love ${lang}!`
-}
+// export function helloWorld(lang = 'Typescript'): string {
+//     return `🦁 I love ${lang}!`
+// }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function devNull(): any {
-    return { hello: 'Efrei' }
-}
+// // eslint-disable-next-line @typescript-eslint/no-explicit-any
+// export function devNull(): any {
+//     return { hello: 'Efrei' }
+// }
 
-console.log(helloWorld())
+// console.log(helloWorld())
+
+import express, { Application, Request, Response } from 'express'
+
+const app: Application = express()
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello')
+})
+
+app.listen(4000, () => console.log('Server running'))
