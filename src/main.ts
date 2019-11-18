d2fc0011417264d084b02b57e5c93521ade62305
Adrien <adrien.masson@hotmail.fr>
2019-11-18 11:51:36 +0100

    init main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 export function helloWorld(lang = 'Typescript'): string {
-  return `🦁 I love ${lang}!`
+    return `🦁 I love ${lang}!`
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function devNull(): any {
-  return { hello: 'Efrei' }
+    return { hello: 'Efrei' }
 }
 
 console.log(helloWorld())
