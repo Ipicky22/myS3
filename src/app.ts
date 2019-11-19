d464c25bf64ea098135ce1315b6471ec2447a688
Adrien <adrien.masson@hotmail.fr>
2019-11-19 09:32:04 +0100

    clean

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import { createConnection } from 'typeorm'
 import { User } from './entity/User'
 
 let createUser = (nickname: string, email: string, password: string): void => {
-
     createConnection()
         .then(async connection => {
             console.log('Inserting a new user into the database...')
@@ -15,13 +14,12 @@ let createUser = (nickname: string, email: string, password: string): void => {
 
             console.log('Saved a new user with id: ' + user.uuid)
             console.log('Loading users from the database...')
-            
+
             const users = await connection.manager.find(User)
 
             console.log('Loaded users: ', users)
-
         })
         .catch(error => console.log(error))
-}   
+}
 
 export default createUser
