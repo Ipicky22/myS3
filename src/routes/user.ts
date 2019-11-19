1493e0b36dd20900124dd5b47a86c525afb4f5af
Adrien <adrien.masson@hotmail.fr>
2019-11-19 15:45:09 +0100

    user crud

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,6 +1,6 @@
 import { Router, Request, Response } from "express";
 import { User } from './../entity/User'
-import { getManager } from 'typeorm'
+import { getManager, getRepository } from 'typeorm'
 const uuid = require('uuid')
 const app = Router();
 
@@ -15,15 +15,46 @@ app.post('/addUser', async (req: Request, res: Response) => {
     user.email = email
     user.password = password
 
-    await getManager().getRepository(User).save(user)
+    await getRepository(User).save(user)
 
     console.log('Saved a new user with id: ' + user.uuid)
     console.log('Loading users from the database...')
-
-    const users = await getManager().find(User)
-
+    const users: User[] | undefined = await getManager().find(User)
     console.log('Loaded users: ', users)
-    res.sendStatus(200).end()
+    res.send(users).end()
+})
+
+app.get('/findAll', async (req: Request, res: Response) => {
+    const users: User[] | undefined = await getManager().find(User)
+    console.log('Users: ', users)
+    res.send(users).end()
+})
+
+app.get('/findOne/:uuid', async (req: Request, res: Response) => {
+    const uuid: string = req.params.uuid
+    const user: User | undefined = await getRepository(User).findOne(uuid)
+    console.log('User: ', user)
+    res.send(user).end()
+})
+
+app.patch('/updateOne/:uuid', async (req: Request, res: Response) => {
+    const uuid: string = req.params.uuid
+    const user: any = await getRepository(User).findOne(uuid)
+    console.log("user", user)
+    getRepository(User).merge(user, req.body)
+    await getRepository(User).save(user)
+    res.send(`User n° ${uuid} has been updated.`).end()
+})
+
+app.delete('/deleteOne/:uuid', async (req: Request, res: Response) => {
+    const uuid: string = req.params.uuid
+    await getRepository(User).delete(uuid)
+    res.send(`User n° ${uuid} has been deleted.`).end()
+})
+
+app.delete('/deleteAll', async (req: Request, res: Response) => {
+    await getRepository(User).clear()
+    res.send(`Users has been deleted.`).end()
 })
 
 export default app;
