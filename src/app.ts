0695f20f92617d604b619d0f2cb0b48ccc13241f
Adrien <adrien.masson@hotmail.fr>
2019-11-19 09:31:50 +0100

    createUser TS

--- src/app.ts
+++ src/app.ts
@@ -2,19 +2,26 @@ import 'reflect-metadata'
 import { createConnection } from 'typeorm'
 import { User } from './entity/User'
 
-createConnection()
-    .then(async connection => {
-        console.log('Inserting a new user into the database...')
-        const user = new User()
-        user.nickname = 'Maxime'
-        user.email = 'coucou@gmail.com'
-        user.password = 'hello'
-        await connection.manager.save(user)
-        console.log('Saved a new user with id: ' + user.uuid)
+let createUser = (nickname: string, email: string, password: string): void => {
 
-        console.log('Loading users from the database...')
-        const users = await connection.manager.find(User)
-        console.log('Loaded users: ', users)
+    createConnection()
+        .then(async connection => {
+            console.log('Inserting a new user into the database...')
+            const user: User = new User()
+            user.nickname = nickname
+            user.email = email
+            user.password = password
+            await connection.manager.save(user)
 
-    })
-    .catch(error => console.log(error))
+            console.log('Saved a new user with id: ' + user.uuid)
+            console.log('Loading users from the database...')
+            
+            const users = await connection.manager.find(User)
+
+            console.log('Loaded users: ', users)
+
+        })
+        .catch(error => console.log(error))
+}   
+
+export default createUser
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express, { Application, Request, Response } from 'express'
-
+import createUser from './app'
 const app: Application = express()
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello')
+})
+
+app.get('/addUser', (req: Request, res: Response) => {
+    //createUser("test", "test@mail.com", "test")
 })
 
 app.listen(4000, () => console.log('Server running'))
