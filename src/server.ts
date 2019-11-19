a7a344d7d1dbcbf6ac7c695a1226ae30f56303e1
Adrien <adrien.masson@hotmail.fr>
2019-11-19 10:29:53 +0100

    user creation

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,18 @@
 import express, { Application, Request, Response } from 'express'
+import bodyParser from 'body-parser'
 import createUser from './app'
 const app: Application = express()
 
-app.get('/', (req: Request, res: Response) => {
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', async function (req: Request, res: Response) {
     res.send('Hello')
 })
 
-app.get('/addUser', (req: Request, res: Response) => {
-    //createUser("test", "test@mail.com", "test")
+app.post('/addUser', async function (req: Request, res: Response) {
+    const { nickname, email, password } = req.body
+    createUser(nickname, email, password)
+    res.sendStatus(200).end()
 })
 
 app.listen(4000, () => console.log('Server running'))
