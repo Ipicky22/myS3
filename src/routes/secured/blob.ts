853bb99a8d6a48253d7f5f50f45052f30d00df1f
Adrien <adrien.masson@hotmail.fr>
2019-12-17 08:41:09 +0100

    TS syntaxe

--- src/routes/secured/blob.ts
+++ src/routes/secured/blob.ts
@@ -3,22 +3,31 @@ import { getRepository, Like } from 'typeorm';
 import Blob from "../../database/entity/Blob";
 import Bucket from "../../database/entity/Bucket"
 import multer from "multer";
-import {createReadStream, createWriteStream, unlinkSync } from "fs";
+import { createReadStream, createWriteStream, unlinkSync } from "fs";
+import fs from 'fs';
 
 const app = Router({
   mergeParams: true
 });
 
 const storage = multer.diskStorage({
-  destination: async(req, file, cb) => {
-    const { uuid, id } = req.params;
-    const bucket = await getRepository(Bucket).findOne(id);
 
+  destination: async (req: Request, file: Response, cb: any, destination: String) => {
+
+    const { uuid, id } = req.params;
+    const bucket : Bucket | undefined = await getRepository(Bucket).findOne(id);
+
+    if ( bucket) {
     await cb(null, process.env.STORAGE + "/" + uuid + "/" + bucket.name)
+    } else {
+      console.log("bucket n'existe pas")
+    }
   },
-  filename: async(req, file, cb) => {
+
+  filename: async (req, file, cb) => {
     await cb(null, file.originalname)
-  }
+  },
+
 });
 
 const upload = multer({ storage })
@@ -29,7 +38,7 @@ app.post("/", upload.single("blob"), async (req: Request, res: Response) => {
     const { filename, path, size } = req.file;
     const { bucket_id } = req.params;
 
-    const blob : Blob = new Blob();
+    const blob: any = new Blob();
 
     blob.name = filename;
     blob.path = path;
@@ -49,75 +58,76 @@ app.post("/", upload.single("blob"), async (req: Request, res: Response) => {
 });
 
 // **************** DELETE Blob **************** //
-app.delete('/:id', async(req: Request, res: Response) => {
+app.delete('/:id', async (req: Request, res: Response) => {
 
-  const id: number = req.params.id;
+  const id: any = req.params.id ;
   const blob: Blob | undefined = await getRepository(Blob).findOne(id);
 
   if (blob) {
     try {
-        fs.unlinkSync(blob.path);
-        console.log(`successfully deleted ${blob.path}`);
-      } catch (err) {
-        console.log(err)
-      }
-      await getRepository(Blob).delete(id);
-      res.status(200).send(`Blob n° ${id} has been deleted.`).end();
+      fs.unlinkSync(blob.path);
+      console.log(`successfully deleted ${blob.path}`);
+    } catch (err) {
+      console.log(err)
+    }
+    await getRepository(Blob).delete(id);
+    res.status(200).send(`Blob n° ${id} has been deleted.`).end();
   } else {
-      res.status(404).send(`Blob n° ${id} was not found.`).end();
+    res.status(404).send(`Blob n° ${id} was not found.`).end();
   }
 
 });
 
 // **************** GET Blob metadata **************** //
-app.get('/metadata/:blob_id', async(req:Request, res: Response) => {
+app.get('/metadata/:blob_id', async (req: Request, res: Response) => {
 
   const { blob_id } = req.params;
-  const blob: Blob | undefined = await getRepository(Blob).findOne(blob_id, { select : ['path','size'] });
+  const blob: Blob | undefined = await getRepository(Blob).findOne(blob_id, { select: ['path', 'size'] });
 
   if (blob) {
-      res.send(blob).end();
+    res.send(blob).end();
   } else {
-      res.send(`Blob n° ${blob_id} was not found.`).end();
+    res.send(`Blob n° ${blob_id} was not found.`).end();
   }
 
 });
 
 // **************** DUPLICATE Blob **************** //
 
-app.get('/duplicate/:blob_id', async(req: Request, res: Response ) => {
+app.get('/duplicate/:blob_id', async (req: Request, res: Response) => {
 
   try {
 
-     const { uuid, bucket_id, blob_id } =  req.params;
-     console.log(req.params)
+    const { uuid, bucket_id, blob_id } = req.params;
+    console.log(req.params)
 
-     const bucket: Bucket | undefined = await getRepository(Bucket).findOne(bucket_id);
-     const blob: Blob | undefined = await getRepository(Blob).findOne(blob_id);
+    const bucket: Bucket | undefined = await getRepository(Bucket).findOne(bucket_id);
+    const blob: Blob | undefined = await getRepository(Blob).findOne(blob_id);
 
-     if (blob) {
+    if (blob) {
 
-       const dir = blob.path.split('.');
-       const regex = '%'+dir[0]+'%';
-       const nbCopy: number = await getRepository(Blob).count({ where: { path: Like(regex) } });
-       const copyField = dir[0] + '.copy.' + nbCopy + '.' + dir[1];
+      const dir = blob.path.split('.');
+      const regex = '%' + dir[0] + '%';
+      const nbCopy: number = await getRepository(Blob).count({ where: { path: Like(regex) } });
+      const copyField = dir[0] + '.copy.' + nbCopy + '.' + dir[1];
 
-       const blobDuplicate: Blob | undefined = new Blob();
-       blobDuplicate.name = blob.name;
-       blobDuplicate.path = copyField;
-       blobDuplicate.size = blob.size;
-       blobDuplicate.bucket = bucket;
+      const blobDuplicate: any = new Blob();
 
-       await getRepository(Blob).save(blobDuplicate);
+      blobDuplicate.name = blob.name;
+      blobDuplicate.path = copyField;
+      blobDuplicate.size = blob.size;
+      blobDuplicate.bucket = bucket;
+      
+      await getRepository(Blob).save(blobDuplicate);
 
-       createReadStream(blob.path).pipe(createWriteStream(copyField));
-       res.status(200).send('duplicate');
+      createReadStream(blob.path).pipe(createWriteStream(copyField));
+      res.status(200).send('duplicate');
 
-     } else {
-       res.status(404).send('Error');
-     }
+    } else {
+      res.status(404).send('Error');
+    }
 
-  } catch(err) {
+  } catch (err) {
     res.status(400).send(err);
   }
 
